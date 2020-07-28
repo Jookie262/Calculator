1368af9c89c8cd45acb86f525ab9d7cc4109000d
JolouTheRookie <barrientos.134038@iloilo.sti.edu.ph>
2020-07-28 17:30:57 +0800

    Clean Code (fixes #1 and fixes #2)

--- app.js
+++ app.js
@@ -1,40 +1,63 @@
+//Start
+
 //fetch objects from HTML
 let formula = document.getElementById("formula");
 let answer = document.getElementById("ans");
-let equal = document.querySelector('eq');
-let click = 0; 
+
+//Global Variables
+let clickOperator = 0; 
+let percent = false;
+let numPercent = 0;
 
 function putNumber(num){
-    let isOperator = (num == '*' || num == "+" || num == "/" || num == '-');
-    if(isOperator){
-        click++;
-        if(!(click >= 2 && isOperator)){
-            click = 1;
-            return formula.value += num;
-        }
-    }else {
-        click = 0;
-        return formula.value += num;
+    if(!percent){
+        clickOperator = 0;
+        formula.value += num;
     }
 }
 
+function putOperator(opt){
+    let forLen = formula.value.length;
+    clickOperator++;
+
+    if(forLen == 0 && opt == '-'){
+        formula.value += '-';
+    }else if(forLen != 0){
+        if(!(clickOperator >= 2)){
+            clickOperator = 1;
+                if(opt == '%'){
+                    numPercent = formula.value;
+                    percent = true;
+                }
+            return formula.value += opt;
+        }
+    }
+}
+
+function getPercentage(num){
+    return num / 100;
+}
+
 function remAll(){
+    percent = false;
     formula.value = "";
     answer.value = 0;
 }
 
 function remOne(){
-    let num = formula.value;
-    let arr = num.split('');
-    let a = arr.pop();
-    if(a == '*' || a == '+' || a == '-' || a == '/'){
-        click = 0;
-    }
-    formula.value = arr.join('');
+    clickOperator = 0;
+    let form = formula.value.split('');
+    let rem = form.pop();
+    if(rem == '%'){ percent = false };
+    formula.value = form.join('');
 }
 
-function answerEQ(){
-   // let getOperator = formula.value.match(/\W/ig);    
-    let ans = eval(formula.value);
-    return answer.value = ans;
+function equals(){
+    try{
+        percent == true ? answer.value = getPercentage(numPercent) : answer.value = eval(formula.value);
+    }catch{
+        ans.value = 'Syntax Error';
+    }
 }
+
+//End
